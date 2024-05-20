d2b51bb136146cdc68d8c4f86add44cdd1912345
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2024-05-20 05:06:42 +0200

    Use normal font for labels and add defines.

--- Source/main.c
+++ Source/main.c
@@ -3,6 +3,14 @@
 #define MAX_LOADSTRING 100
 #define TIMER_IDENT 0xBEEF
 
+#define WINDOW_WIDTH 350
+#define WINDOW_HEIGHT 200
+
+#define LABEL_WIDTH 300
+#define LABEL_HEIGHT 32
+
+#define FORMAT_BUFFER_SIZE 48
+
 static HINSTANCE hInst;
 static TCHAR szTitle[MAX_LOADSTRING];
 static TCHAR szWindowClass[MAX_LOADSTRING];
@@ -12,9 +20,11 @@ static TCHAR szExitText[MAX_LOADSTRING];
 static DWORD SecondsPassed = 0;
 static HWND SecondsLabelHwnd = NULL;
 static HWND ChicagoLabelHwnd = NULL;
+static HFONT GuiFont = NULL;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK AboutProc(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK SetFont(HWND, LPARAM);
 
 DWORD APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, DWORD nCmdShow)
 {
@@ -36,7 +46,7 @@ DWORD APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmd
 		.hInstance = hInstance,
 		.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CHICAGOSAMPLE)),
 		.hCursor = LoadCursor(NULL, IDC_ARROW),
-		.hbrBackground = (HBRUSH)COLOR_BACKGROUND,
+		.hbrBackground = GetSysColorBrush(COLOR_BTNFACE),
 		.lpszMenuName = MAKEINTRESOURCE(IDC_CHICAGOSAMPLE),
 		.lpszClassName = szWindowClass,
 		.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL))
@@ -44,14 +54,14 @@ DWORD APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmd
 
 	RegisterClassEx(&wcex);
 
-	HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+	HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
 	hInst = hInstance;
 
 	if (!hWnd)
 		return FALSE;
 
-	ChicagoLabelHwnd = CreateWindow("STATIC", "Compiled with the Windows 11 SDK and ChicagoCRT!", WS_VISIBLE | WS_CHILD | SS_LEFT, 10, 10, 300, 32, hWnd, NULL, hInstance, NULL);
-	SecondsLabelHwnd = CreateWindow("STATIC", "0 seconds have passed since opening!", WS_VISIBLE | WS_CHILD | SS_LEFT, 10, 50, 300, 32, hWnd, NULL, hInstance, NULL);
+	ChicagoLabelHwnd = CreateWindow("STATIC", "Compiled with the Windows 11 SDK and ChicagoCRT!", WS_VISIBLE | WS_CHILD | SS_LEFT, 10, 10, LABEL_WIDTH, LABEL_HEIGHT, hWnd, NULL, hInstance, NULL);
+	SecondsLabelHwnd = CreateWindow("STATIC", "0 seconds have passed since opening!", WS_VISIBLE | WS_CHILD | SS_LEFT, 10, 50, LABEL_WIDTH, LABEL_HEIGHT, hWnd, NULL, hInstance, NULL);
 
 	SetTimer(hWnd, TIMER_IDENT, 1000, NULL);
 
@@ -59,6 +69,17 @@ DWORD APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmd
 	UpdateWindow(hWnd);
 
 	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CHICAGOSAMPLE));
+
+	// Set the font for all child elements.
+	NONCLIENTMETRICS metrics;
+	RtlZeroMemory(&metrics, sizeof(NONCLIENTMETRICS));
+
+	metrics.cbSize = sizeof(metrics);
+	SystemParametersInfo(SPI_GETNONCLIENTMETRICS, metrics.cbSize, &metrics, 0);
+
+	GuiFont = CreateFontIndirect(&metrics.lfMessageFont);
+
+	EnumChildWindows(hWnd, (WNDENUMPROC)SetFont, (LPARAM)GuiFont);
 
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0))
@@ -71,6 +92,14 @@ DWORD APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmd
 	}
 
 	return (int)msg.wParam;
+}
+
+// Set the font for a control.
+BOOL CALLBACK SetFont(HWND hWnd, LPARAM font)
+{
+	SendMessage(hWnd, WM_SETFONT, font, TRUE);
+
+    return TRUE;
 }
 
 // Process the messages for the main window.
@@ -108,8 +137,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_DESTROY:
+		if(GuiFont) DeleteObject(GuiFont);
+
 		KillTimer(hWnd, TIMER_IDENT);
 		PostQuitMessage(0);
+
 		break;
 	case WM_TIMER:
 		switch (wParam)
@@ -117,8 +149,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		case TIMER_IDENT:
 			SecondsPassed++;
 
-			CHAR buff[48];
-			RtlZeroMemory(buff, 48);
+			CHAR buff[FORMAT_BUFFER_SIZE];
+			RtlZeroMemory(buff, FORMAT_BUFFER_SIZE);
 
 			sprintf(buff, "%d seconds have passed since opening!", SecondsPassed);
 
